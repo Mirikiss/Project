215e9e8132d467f9ef12acc3f3b5599fdb51f2d4
Pavel <limit1993@mail.ru>
2024-02-15 21:19:14 +0300

    project

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from django.urls import include
+from . import views
+from .views import withyou
+
+app_name = 'blog'
 
 urlpatterns = [
-    path('blog/', include('blog.urls', namespace='blog'))
-]
+    path('', views.PostView.as_view(), name='index'),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('review/<int:pk>/', views.AddComments.as_view(), name='add_comments'),
+    path('<int:pk>/add_likes', views.AddLike.as_view(), name='add_likes'),
+    path('<int:pk>/del_likes', views.DelLike.as_view(), name='del_likes'),
+    path('with_you/', withyou, name='with_you')]
